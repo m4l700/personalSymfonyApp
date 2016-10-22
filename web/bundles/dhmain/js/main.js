1d97138e3e69c46887716c57df132ae2ce1bae4f
Your Name <d.holleman700@gmail.com>
2016-10-22 17:24:15 +0200

    [Unstable] Additional frontend fixes

--- web/bundles/dhmain/js/main.js
+++ web/bundles/dhmain/js/main.js
@@ -1,19 +1,30 @@
 $(document).ready(function () {
 
-//Smooth scroll to anchor
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
+    //Smooth scroll to anchor
+    $(document).on('click', 'a', function(event){
+        event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-});
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 700);
+    });
 
-//Add and remove active class
-$('.dhNav').click(function(){
-    $('.active').removeClass('active');
-    $(this).addClass('active');
-});
+    //Add and remove class based on scroll position
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+
+        //if (scroll >= 500){
+            //$('.fa-home').removeClass('active');
+          //  $('.fa-home').addClass('active');
+        //}
+    })
+
+
+    //Add and remove active class
+    $('.dhNav').click(function(){
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+    });
 
 
 
