397569191c78b68bc9d365f8d5881bb9892336df
Your Name <d.holleman700@gmail.com>
2016-10-22 23:08:40 +0200

    [Unstable] WIP font/backend fixes

--- web/bundles/dhmain/js/main.js
+++ web/bundles/dhmain/js/main.js
@@ -1,23 +1,38 @@
 $(document).ready(function () {
 
     //Smooth scroll to anchor
-    $(document).on('click', 'a', function(event){
-        event.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top
-        }, 700);
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 700);
+            return false;
+          }
+        }
+      });
     });
 
+    //$(document).on('click', 'a', function(event){
+    //    event.preventDefault();
+
+    //    $('.sectionStyle').animate({
+    //        scrollTop: $( $.attr(this, 'href') ).offset().top
+    //    }, 700);
+    //});
+
     //Add and remove class based on scroll position
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
+    //$(window).scroll(function(){
+    //   var scroll = $(window).scrollTop();
 
         //if (scroll >= 500){
             //$('.fa-home').removeClass('active');
           //  $('.fa-home').addClass('active');
         //}
-    })
+    //})
 
 
     //Add and remove active class
@@ -26,6 +41,10 @@ $(document).ready(function () {
         $(this).addClass('active');
     });
 
+
+    $("#container:first-child").mousemove(function(e){
+      $('.sectionStyle .frontPage').css({'top': e.clientY - 20, 'left': e.clientX - 20});
+    });
 
 
 });
